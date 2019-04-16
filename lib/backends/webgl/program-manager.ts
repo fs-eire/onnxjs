cb97bcfdaee87ed048166352dd68cf75c240dd4a
Yulong Wang <f.s@qq.com>
2019-04-16 15:15:42 -0700

    normalize types

--- lib/backends/webgl/program-manager.ts
+++ lib/backends/webgl/program-manager.ts
@@ -5,51 +5,9 @@ import {env} from '../../env';
 import {Logger, Profiler} from '../../instrument';
 
 import {GlslPreprocessor} from './glsl-preprocessor';
-import {ProgramInfo} from './program-info';
-import {TextureData} from './texture-data';
+import {Artifact, LocationInfo, ProgramInfo, RunData, TextureData, UniformData, VariableInfo} from './types';
 import {WebGLContext} from './webgl-context';
 
-/**
- * Information extracted from Shader source to help with binding later
- */
-export class VariableInfo {
-  type: string;
-  name: string;
-  isVec: boolean;
-  arraySuffix?: string;
-}
-/**
- * LocationInfo contains a mappig from a variable name (inside shader)
- * to its "location" in the compiled program
- */
-export class LocationInfo {
-  variable: VariableInfo;
-  location: WebGLUniformLocation|number;
-}
-/**
- * Artifact is the result of compilation
- * It does not contain input of output data
- * However anything that could be run as a "program"
- */
-export interface Artifact {
-  programInfo: ProgramInfo;
-  program: WebGLProgram;
-  uniformLocations: {[name: string]: LocationInfo};
-  attribLocations: {[name: string]: LocationInfo};
-}
-
-export interface UniformData {
-  [name: string]: number|number[];
-}
-
-export class RunData {
-  inputTextureDatas: TextureData[];
-  outputTextureData: TextureData;
-  uniformData: UniformData;
-  preRun?: (glContext: WebGLContext, artifact: Artifact) => void;
-  postRun?: (glContext: WebGLContext, artifact: Artifact) => void;
-  draw?: (glContext: WebGLContext, artifact: Artifact) => void;
-}
 /**
  * ProgramManager is the main class behind running computations
  * It builds ProgramInfo's into Artifacts
@@ -67,7 +25,6 @@ export class ProgramManager {
 
   constructor(public profiler: Readonly<Profiler>, public glContext: WebGLContext) {
     this.repo = new Map();
-    this.glContext = glContext;
     this.attributesBound = false;
   }
   getArtifact(key: {}): Artifact|undefined {
@@ -262,20 +219,14 @@ ${fragShaderScript}
   getAttribLocations(program: WebGLProgram, variableInfos: VariableInfo[]): {[name: string]: LocationInfo} {
     const locationInfos: {[name: string]: LocationInfo} = {};
     variableInfos.forEach(vi => {
-      const li = new LocationInfo();
-      li.variable = vi;
-      li.location = this.getAttribLocation(program, vi.name);
-      locationInfos[vi.name] = li;
+      locationInfos[vi.name] = {variable: vi, location: this.getAttribLocation(program, vi.name)};
     });
     return locationInfos;
   }
   getUniformLocations(program: WebGLProgram, variableInfos: VariableInfo[]): {[name: string]: LocationInfo} {
     const locationInfos: {[name: string]: LocationInfo} = {};
     variableInfos.forEach(vi => {
-      const li = new LocationInfo();
-      li.variable = vi;
-      li.location = this.getUniformLocation(program, vi.name);
-      locationInfos[vi.name] = li;
+      locationInfos[vi.name] = {variable: vi, location: this.getUniformLocation(program, vi.name)};
     });
     return locationInfos;
   }
